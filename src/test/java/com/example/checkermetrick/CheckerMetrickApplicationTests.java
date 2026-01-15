3b9954df8ea594e4e983445b8aee32d19576199c
IAmOkey <artem.to2018@yandex.ru>
2026-01-15 19:56:43 +0300

    first

---
+++ src/test/java/com/example/checkermetrick/CheckerMetrickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.checkermetrick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckerMetrickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
